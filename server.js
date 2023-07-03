27678bc19204f39bae6fee4d3fdefccee25264bc
Prateek Kumar Sahu <77916342+proPrateekSahu@users.noreply.github.com>
2023-07-03 19:09:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,17 +19,13 @@ const app = express();
 
 //middlewares
 app.use(express.json());
-<<<<<<< HEAD
+
 app.use(
   cors({
     origin: ['http://localhost:8080', 'https://mernbloodbankapp.onrender.com/'],
   })
 );
-=======
-app.use(cors({
-    origin: ['http://localhost:8080', 'https://mernbloodbankapp.onrender.com/'],
-  }));
->>>>>>> 435bf0f565036430c5b7bc8affa95c50b29809ac
+
 app.use(morgan('dev'));
 
 //routes
